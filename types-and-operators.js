3d6059a12f0473ff74adf1eeb3c214a4e7a8cd58
Asgarov Asgar <asgara@maestro.st>
2023-10-24 23:04:40 +0400

    reviewed types and operators

---
+++ types-and-operators.js
@@ -0,0 +1,26 @@
+console.log('type');
+// static vs dynamic typing
+
+// primitive - variable can hold only one value
+// 1. undefined represents lack of existence (cannot set variable)
+// 2. null represents lack of existence (can set variable)
+// 3. number - floating number
+// 4. boolean
+// 5. string
+// 6. symbol
+
+// OPERATOR - a special function that is syntactically (written) differently
+var a = 3 + 4
+// function +(a,b) {
+//   return add operation somewhere
+// }
+
+// 3 + 4 this is infix notation
+// + 3 4 this is prefix notation
+// +(3, 4) standar function
+
+// OPERATOR PRECEDENCE - which operator function gets called first
+// functions are called in order of precedence
+// COERCION - converting a value from one type to another
+// Number(undefined) => NaN
+// Number(null) => 0
