d3ff5380ad089c82c472a0f26cccac0a800f903a
Asgarov Asgar <asgara@maestro.st>
2023-11-01 23:13:09 +0400

    completed exploring jquery

--- explore-jquery.js
+++ explore-jquery.js
@@ -1,1 +1,6 @@
 console.log('Explore JQuery');
+var q = $('ul.people').addClass('hello').removeClass('people')
+console.log(q);
+
+// METHOD CHAINING - calling one method after another, 
+// and each method affects the parent object
